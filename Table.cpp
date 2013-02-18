bdf555c0802473a86077be774d460a9f3d42087a
Austin Dalton <dalt366@tamu.edu>
2013-02-18 14:44:23 -0600

    Adding in Table.cpp for the testing

---
+++ Table.cpp
@@ -0,0 +1,51 @@
+#include "Table.h"
+
+void Table::add(string attributeName, TYPE type){
+	
+}
+
+void Table::deleteFunction(string attributeName){
+
+}
+
+void Table::insert(Record record){
+
+}
+/*
+map<string,Table::TYPE> Table::getAttributes(){
+	 map<"hello",Table::STRING> theMap;
+    
+    return theMap;
+}*/
+
+int Table::getSize(){
+	return 2;
+}
+
+Record Table::getRecord(int rowNumber){
+
+}
+
+void Table::renameAttribute(string oldName, string newName){
+
+}
+
+Table Table::crossJoin(Table table1, Table table2){
+
+}
+
+float Table::getSum(string attributeName){
+	return 2;
+}
+
+int Table::getCount(string attributeName){
+	return 2;
+}
+
+float Table::getMin(string attributeName){
+	return 2;
+}
+
+float Table::getMax(string attributeName){
+	return 2;
+}
