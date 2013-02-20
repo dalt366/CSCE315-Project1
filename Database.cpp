711c538f22f214210a102dcbfcf40db93dd34015
Austin Dalton <dalt366@tamu.edu>
2013-02-20 14:08:56 -0600

    adding the database.cpp shell

---
+++ Database.cpp
@@ -0,0 +1,25 @@
+#include "Database.h"
+
+bool Database::addTable(Table table, string name){
+	return true;
+}
+
+bool Database::dropTable(string name){
+	return true;
+}
+
+vector<Table> Database::getTables(){
+	return tables;
+}
+
+vector<string> Database::listTables(){
+	
+}
+
+Table Database::evalQuery(string query){
+	
+}
+
+void Database::evalDeleteQuery(string query){
+
+}
