b5be3237820e320c60bbd50c644f3c7d5e2a5051
Brent Walther <brentwalther@gmail.com>
2013-02-25 14:15:38 -0600

    Implemented a couple things in Application
    Implemented a couple things.

--- DatabastApplication/DatabastApplication/Application.cpp
+++ DatabastApplication/DatabastApplication/Application.cpp
@@ -1,11 +1,21 @@
 #include "Database.h"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
 Table loadData(string filename); 
 
 int main() {
+	//Initialize the database
+	Database database = Database();
+
+	//Add the tables
+	database.addTable(loadData("userprofile.csv"), "Customers");
 }
 
 Table loadData(string filename) {
+	//TODO - build a table from the csv file.
+	Table table = Table();
+	return table;
 }
