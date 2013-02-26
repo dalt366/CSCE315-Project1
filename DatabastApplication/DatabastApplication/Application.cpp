477fbfb463bc86d49f8292bc3fdc58bd5f977df3
Brent Walther <brentwalther@gmail.com>
2013-02-25 21:48:08 -0600

    Changed LibraryTest and Application
    Changed the library testing and conluded that the other teams library is
    not suitable to use. Email sent to TA. Did a little more implementation
    on Application: loadData() function, main menu function, main function.

--- DatabastApplication/DatabastApplication/Application.cpp
+++ DatabastApplication/DatabastApplication/Application.cpp
@@ -4,20 +4,80 @@
 
 using namespace std;
 
-Table loadData(string filename); 
+Table loadData(string filename); // Loads a table from a .csv
+int getMenuSelection(); // Shows the menu and gets the choice
 
 int main() {
 	//Initialize the database
 	Database database = Database();
 
 	//Add the tables
+	database.addTable(loadData("chefmozaccepts.csv"), "Payments");
+	database.addTable(loadData("chefmozcuisine.csv"), "Cuisines");
+	database.addTable(loadData("chefmozparking.csv"), "Parking");
+	database.addTable(loadData("geoplaces2.csv"), "Restaurants");
 	database.addTable(loadData("userprofile.csv"), "Customers");
+	database.addTable(loadData("rating_final.csv"), "Ratings");
+
+	for(;;) {
+		int choice = getMenuSelection();
+		switch(choice) {
+		case 1:
+			break;
+		case 2:
+			break;
+		default:
+			cout << "Invalid choice" << endl;
+		}
+		if(choice == 0) {
+			break;
+		}
+	}
+	return 0; // Program exit 
 }
+
+int getMenuSelection() {
+	cout << "MAIN MENU" << endl;
+	cout << "---------" << endl;
+	cout << "\t" << "1) Do cool stuff" << endl;
+	cout << "\t" << "2) Do something else" << endl;
+	cout << "\t" << "0) Exit" << endl;
+	cout << "Enter your selection (number): ";
+	int choice;
+	cin >> choice;
+	return choice;
+}
+
 
 Table loadData(string filename) {
 	//TODO - build a table from the csv file.
 	ifstream file = ifstream(filename);
 	Table table = Table();
+	string line;
+	getline(file, line);
+
+	//Fill the attributes
+	stringstream sstream(line);
+	string item;
+	while(getline(sstream, item, ',')) {
+		table.addAttribute(item, Table::Type::string);
+	}
+
+	//Fill the table with records
+	while(getline(file, line)) {
+		Record record = Record();
+
+		stringstream sstream(line);
+		string item;
+		int index = 0;
+		while(getline(sstream, item, ',')) {
+			record.modifyRecordEntry(index++, item);
+		}
+
+		table.insertRecord(record);
+	}
+
+	// Close and return
 	file.close();
 	return table;
 }
--- LibraryTesting/LibraryTesting/unit_tests.cpp
+++ LibraryTesting/LibraryTesting/unit_tests.cpp
@@ -4,150 +4,157 @@
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Main
-#include <./boost/test/unit_test.hpp>
+//#include <./boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( my_test )
+//BOOST_AUTO_TEST_CASE( my_test )
+int main()
 {
     Database * test_db = new Database();
 	Table * test_table1 = new Table();
 	Table * test_table2 = new Table();
 
-    BOOST_CHECK( test_db != NULL );
-	BOOST_CHECK( test_table1 != NULL);
-	BOOST_CHECK( test_table2 != NULL);
+    printf("test is %d\n", test_db != NULL );
+	printf("test is %d\n", test_table1 != NULL);
+	printf("test is %d\n", test_table2 != NULL);
 	
 	test_db->addTable(*test_table1, "Table 1");
 
-	BOOST_CHECK(test_db->listTables().size() == 1);
-	BOOST_CHECK(test_db->getTables().front() == *test_table1);
-	BOOST_CHECK(test_db->getTables().front() == *test_table2); // Valid because both tables have no attributes or records
-	BOOST_CHECK(test_db->listTables().front() == "Table 1");
-	BOOST_CHECK(test_db->dropTable("Table 2"));
-	BOOST_CHECK(test_db->listTables().size() == 1);
-	BOOST_CHECK(test_db->dropTable("Table 1"));
-	BOOST_CHECK(test_db->listTables().size() == 0);
+	printf("test is %d\n",test_db->listTables().size() == 1);
+	printf("test is %d\n",test_db->listTables().front() == "Table 1");
+	test_db->dropTable("Table 2");
+	printf("test is %d\n",test_db->listTables().size() == 1);
+	test_db->dropTable("Table 1");
+	printf("test is %d\n",test_db->listTables().size() == 0);
 
 	test_db->addTable(*test_table2, "Table 2");
 
-	BOOST_CHECK(test_db->listTables().size() == 1);
-	BOOST_CHECK(test_db->listTables().front() == "Table 2");
+	printf("test is %d\n",test_db->listTables().size() == 1);
+	printf("test is %d\n",test_db->listTables().front() == "Table 2");
 
 	Table * test_table3 = new Table();
-	BOOST_CHECK(test_table3->getAttributes().size() == 0);
+	printf("test is %d\n",test_table3->getAttributes().size() == 0);
 
-	test_table3->addAttribute("key", INT);
-	test_table3->addAttribute("fName", string);
-	test_table3->addAttribute("lName", string);
-	test_table3->addAttribute("bogus", FLOAT);
-	BOOST_CHECK(test_table3->deleteAttribute("BOGUS"));
+	test_table3->addAttribute("key", Table::Type::INT);
+	test_table3->addAttribute("fName", Table::Type::string);
+	test_table3->addAttribute("lName", Table::Type::string);
+	test_table3->addAttribute("bogus", Table::Type::FLOAT);
+	printf("test is %d\n",test_table3->deleteAttribute("BOGUS"));
 
 	test_db->addTable(*test_table3, "Table 3");
-	BOOST_CHECK(test_db->listTables().size() == 2);
-	BOOST_CHECK(test_table3->getAttributes().size() == 4);
-	BOOST_CHECK(test_db->getTables().back().getAttributes().size() == 4);
+	printf("test is %d\n",test_db->listTables().size() == 2);
+	printf("test is %d\n",test_table3->getAttributes().size() == 4);
+	printf("test is %d\n",test_db->getTables().back().getAttributes().size() == 4);
 
 	Table myTestTable = test_db->getTables().back();
 
-	BOOST_CHECK(myTestTable.getAttributes().size() == 4);
-	BOOST_CHECK(myTestTable.renameAttribute("bogus", "unreal"));
-	BOOST_CHECK(myTestTable.getAttributes().size() == 4);
-	BOOST_CHECK(!myTestTable.deleteAttribute("bogus"));
-	BOOST_CHECK(myTestTable.deleteAttribute("unreal"));	
-	BOOST_CHECK(myTestTable.getAttributes().size() == 3);
-	BOOST_CHECK(myTestTable.getSize() == 0);
-	BOOST_CHECK(myTestTable.getCount("fName") == 0);
+	printf("test is %d\n",myTestTable.getAttributes().size() == 4);
+	printf("test is %d\n",myTestTable.renameAttribute("bogus", "unreal"));
+	printf("test is %d\n",myTestTable.getAttributes().size() == 4);
+	printf("test is %d\n",!myTestTable.deleteAttribute("bogus"));
+	printf("test is %d\n",myTestTable.deleteAttribute("unreal"));	
+	printf("test is %d\n",myTestTable.getAttributes().size() == 3);
+	printf("test is %d\n",myTestTable.getSize() == 0);
+	printf("test is %d\n",myTestTable.getCount("fName") == 0);
 
-	test_table3->addAttribute("num", FLOAT);
+	test_table3->addAttribute("num", Table::Type::FLOAT);
 
-	Record test_record1();
+	Record test_record1 = Record();
 	test_record1.modifyRecordEntry(0, "1"); // should be key?
 	test_record1.modifyRecordEntry(1, "brent"); // should be fName?
 	test_record1.modifyRecordEntry(2, "walther"); // should be lName?
 	test_record1.modifyRecordEntry(3, "12.12");
 
-	Record test_record2();
+	Record test_record2 = Record();
 	test_record2.modifyRecordEntry(0, "2"); // should be key?
 	test_record2.modifyRecordEntry(1, ""); // should be fName?
 	test_record2.modifyRecordEntry(2, "person"); // should be lName?
 	test_record2.modifyRecordEntry(3, "14.14");
 
-	Record test_record3();
+	Record test_record3 = Record();
 	test_record3.modifyRecordEntry(0, "3"); // should be key?
 	test_record3.modifyRecordEntry(1, "another"); // should be fName?
 	test_record3.modifyRecordEntry(2, "test"); // should be lName?
 	test_record3.modifyRecordEntry(3, "");
 
-	Record test_record4();
+	Record test_record4 = Record();
 	test_record4.modifyRecordEntry(0, "4"); // should be key?
 	test_record4.modifyRecordEntry(1, "good"); // should be fName?
 	test_record4.modifyRecordEntry(2, "guygreg"); // should be lName?
 	test_record4.modifyRecordEntry(3, " 13.13");
 
-	BOOST_CHECK(test_record1.accessRecordEntry(0) == "1");
-	BOOST_CHECK(test_record1.accessRecordEntry(2) == "walther");
-	BOOST_CHECK(test_record1.accessRecordEntry(4) == NULL);
+	printf("test is %d\n",(*test_record1.accessRecordEntry(0)).compare("1") == 0);
+	printf("test is %d\n",(*test_record1.accessRecordEntry(2)).compare("walther") == 0);
+	printf("test is %d\n",test_record1.accessRecordEntry(4) == NULL);
 
 	myTestTable.insertRecord(test_record1);
 	myTestTable.insertRecord(test_record2);
 	myTestTable.insertRecord(test_record3);
 
-	BOOST_CHECK(test_db->getTables().back().getSize() == 3);
-	BOOST_CHECK(test_db->getTables().back().getSum("num") == 36.36);
-	BOOST_CHECK(test_db->getTables().back().getCount("fName") == 2);
-	BOOST_CHECK(test_db->getTables().back().getCount("lName") == 3);
-	BOOST_CHECK(test_db->getTables().back().getMin("num") == 12.12);
-	BOOST_CHECK(test_db->getTables().back().getMax("num") == 14.14);
+	printf("test is %d\n",test_db->getTables().back().getSize() == 3);
+	printf("test is %d\n",test_db->getTables().back().getSum("num") == 36.36);
+	printf("test is %d\n",test_db->getTables().back().getCount("fName") == 2);
+	printf("test is %d\n",test_db->getTables().back().getCount("lName") == 3);
+	printf("test is %d\n",test_db->getTables().back().getMin("num") == 12.12);
+	printf("test is %d\n",test_db->getTables().back().getMax("num") == 14.14);
 
 	myTestTable.insertRecord(test_record3);
-	BOOST_CHECK(test_db->getTables().back().getSize() == 4);
-	BOOST_CHECK(test_db->getTables().back().getSum("num") == 49.49);
-	BOOST_CHECK(test_db->getTables().back().getCount("fName") == 3);
-	BOOST_CHECK(test_db->getTables().back().getCount("lName") == 4);
-	BOOST_CHECK(test_db->getTables().back().getMin("num") == 12.12);
-	BOOST_CHECK(test_db->getTables().back().getMax("num") == 14.14);
+	printf("test is %d\n",test_db->getTables().back().getSize() == 4);
+	printf("test is %d\n",test_db->getTables().back().getSum("num") == 49.49);
+	printf("test is %d\n",test_db->getTables().back().getCount("fName") == 3);
+	printf("test is %d\n",test_db->getTables().back().getCount("lName") == 4);
+	printf("test is %d\n",test_db->getTables().back().getMin("num") == 12.12);
+	printf("test is %d\n",test_db->getTables().back().getMax("num") == 14.14);
 
-	Table combined_test_table = test_table3.crossJoin(test_table3, test_table2);
+	Table combined_test_table = test_table3->crossJoin(*test_table3, *test_table2);
 
-	BOOST_CHECK(combined_test_table.getSize() == test_table3.getSize());
+	printf("test is %d\n",combined_test_table.getSize() == test_table3->getSize());
 
-	Record test_record5();
+	Record test_record5 = Record();
 	test_record5.modifyRecordEntry(0, "5"); // should be key?
 	test_record5.modifyRecordEntry(1, "marshall"); // should be fName?
 	test_record5.modifyRecordEntry(2, "mathers"); // should be lName?
 	test_record5.modifyRecordEntry(3, "1.11");
 
-	test_table2.insertRecord(test_record5);
-	Table combined_test_table = test_table3.crossJoin(test_table3, test_table2);
+	test_table2->insertRecord(test_record5);
+	combined_test_table = test_table3->crossJoin(*test_table3, *test_table2);
 
-	BOOST_CHECK(combined_test_table.getSize() != test_table3.getSize());
-	BOOST_CHECK(combined_test_table.getSize() == 5);	
-	BOOST_CHECK(test_db->getTables().back().getSum("num") == 50.60);
-	BOOST_CHECK(test_db->getTables().back().renameAttribute("num", "aNumber"));
-	BOOST_CHECK(test_db->getTables().back().getSum("aNumber") == 50.60);
-	BOOST_CHECK(test_db->getTables().back().getSum("num") == 0);
+	printf("test is %d\n",combined_test_table.getSize() != (*test_table3).getSize());
+	printf("test is %d\n",combined_test_table.getSize() == 5);	
+	printf("test is %d\n",test_db->getTables().back().getSum("num") == 50.60);
+	printf("test is %d\n",test_db->getTables().back().renameAttribute("num", "aNumber"));
+	printf("test is %d\n",test_db->getTables().back().getSum("aNumber") == 50.60);
+	printf("test is %d\n",test_db->getTables().back().getSum("num") == 0);
 
-	BOOST_CHECK(test_db->query("fName, lName","Table3","1==1").getSize() == 5);
-	BOOST_CHECK(test_db->query("fName, lName","Table3","1==0").getSize() == 0);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","key >= 2 AND key < 2").getSize() == 5);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","key >= 2").getSize() == 4);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","key > 2").getSize() == 3);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","key <= 2").getSize() == 2);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","key < 2").getSize() == 1);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","fName != \"brent\" OR lName != \"mathers\"").getSize() == 3);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","fName != \"brent\" AND lName != \"mathers\"").getSize() == 3);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","NOT fName == \"brent\" AND NOT lName == \"mathers\"").getSize() == 3);
+	std::list<std::string> attributeList = std::list<std::string>();
+	attributeList.push_back("fName");
+	attributeList.push_back("lName");
+
+	std::list<std::string> attributeList2 = std::list<std::string>();
+	attributeList.push_back("fName");
+	attributeList.push_back("aNumber");
+
+	printf("test is %d\n",test_db->query(attributeList,"Table3","1==1").getSize() == 5);
+	printf("test is %d\n",test_db->query(attributeList,"Table3","1==0").getSize() == 0);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","key >= 2 AND key < 2").getSize() == 5);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","key >= 2").getSize() == 4);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","key > 2").getSize() == 3);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","key <= 2").getSize() == 2);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","key < 2").getSize() == 1);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","fName != \"brent\" OR lName != \"mathers\"").getSize() == 3);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","fName != \"brent\" AND lName != \"mathers\"").getSize() == 3);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","NOT fName == \"brent\" AND NOT lName == \"mathers\"").getSize() == 3);
 
 	test_db->deleteRecord("Table3", "key < 2");
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","fName != \"brent\" OR lName != \"mathers\"").getSize() == 3);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","fName != \"brent\" AND lName != \"mathers\"").getSize() == 3);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","NOT fName == \"brent\" AND NOT lName == \"mathers\"").getSize() == 3);	
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","key < 2").getSize() == 0);
-	BOOST_CHECK(test_db->query("fName, aNumber","Table3","fName == \"brent\"").getSize() == 0);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","fName != \"brent\" OR lName != \"mathers\"").getSize() == 3);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","fName != \"brent\" AND lName != \"mathers\"").getSize() == 3);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","NOT fName == \"brent\" AND NOT lName == \"mathers\"").getSize() == 3);	
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","key < 2").getSize() == 0);
+	printf("test is %d\n",test_db->query(attributeList2,"Table3","fName == \"brent\"").getSize() == 0);
 
-	test_db->addTable(test_db->query("fName, aNumber","Table3","fName != \"brent\" OR lName != \"mathers\""), "myQuery");
-	BOOST_CHECK(test_db->listTables().size() == 3);
+	test_db->addTable(test_db->query(attributeList2,"Table3","fName != \"brent\" OR lName != \"mathers\""), "myQuery");
+	printf("test is %d\n",test_db->listTables().size() == 3);
 	test_db->dropTable("myQuery");
-	BOOST_CHECK(test_db->listTables().size() == 2);
+	printf("test is %d\n",test_db->listTables().size() == 2);
 
 	Record *test_rd1 = new Record();
 
@@ -156,16 +163,10 @@ BOOST_AUTO_TEST_CASE( my_test )
 	test_rd1->modifyRecordEntry(4, "WHATISTHIS>>?");
 	test_rd1->modifyRecordEntry(3, "IMPOSSIBLE");
 
-	BOOST_CHECK(test_rd1->accessRecordEntry(1) == "N");
-	BOOST_CHECK(test_rd1->accessRecordEntry(1) != "X");
-	BOOST_CHECK(test_rd1->accessRecordEntry(2) == "WHATISTHAT>>??");
-	BOOST_CHECK(test_rd1->accessRecordEntry(5) == "IMPOSSIBLE"); //67
-
 	Table * test_table9000 = new Table();
 	test_db->addTable(*test_table9000, "Table9000");
 	test_db->addTable(*test_table9000, "Table9000");
 	test_db->dropTable("Table9000");	
 	test_db->dropTable("Table9000");	
-	BOOST_CHECK(test_db->listTables().size() == 2);
-}
+	printf("test is %d\n",test_db->listTables().size() == 2);
 }
