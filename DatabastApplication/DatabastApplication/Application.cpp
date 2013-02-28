a850762f4bace59827bc108b7f8907dfe60fdbf0
Brent Walther <brentwalther@gmail.com>
2013-02-28 14:55:27 -0600

    Application structure rewrite
    -Rewrote the application structure, making all the functions static
    members of an "Application" class. The main() function accesses them all
    statically.
    -Cleaned up some formatting on the outputs of my queries
    -Cleaned up the menu function
    -Added two optional requirements. One gives a breakdown of restaurant
    cuisines, one lists restaurants by cuisine type. The latter is buggy --
    I can't get it to display the restaurant name, only the placeID.

--- DatabastApplication/DatabastApplication/Application.cpp
+++ DatabastApplication/DatabastApplication/Application.cpp
@@ -1,20 +1,9 @@
-#include "API.h"
+#include "Application.h"
 #include <fstream>
 #include <iostream>
 #include <sstream>
 
 using namespace std;
-
-void loadData(Table& destTable, string filename); // Loads a table from a .csv
-int getMenuSelection(); // Shows the menu and gets the choice
-void printTables(Database &db);
-void printRecords(Database &db, string tableName);
-void printCustomerInfo(Database &db, string customerName);
-void printRestaurantInfo(Database &db, string restaurantName);
-void printRestaurantRatings(Database &db, string restaurantName);
-void customerBreakdown(Database &db);
-void printCustomerRatings(Database &db, string customerName);
-enum MENU { EXIT, CUSTOMER_INFO, RESTAURANT_INFO, RESTAURANT_RATINGS, CUSTOMER_BREAKDOWN, CUSTOMER_RATINGS };
 
 int main() {
 	//Initialize the database
@@ -22,35 +11,35 @@ int main() {
 
 	//Add the tables
 	Table * table = new Table();
-	loadData(*table, "./data/chefmozaccepts.csv");
+	Application::loadData(*table, "./data/chefmozaccepts.csv");
 	database->addTable(*table, "Payments");
 
 	table = new Table();
-	loadData(*table, "./data/chefmozcuisine.csv");
+	Application::loadData(*table, "./data/chefmozcuisine.csv");
 	database->addTable(*table, "Cuisines");
 
 	table = new Table();
-	loadData(*table, "./data/chefmozparking.csv");
+	Application::loadData(*table, "./data/chefmozparking.csv");
 	database->addTable(*table, "Parking");
 
 	table = new Table();
-	loadData(*table, "./data/geoplaces2.csv");
+	Application::loadData(*table, "./data/geoplaces2.csv");
 	database->addTable(*table, "Restaurants");
 
 	table = new Table();
-	loadData(*table, "./data/userprofile.csv");
+	Application::loadData(*table, "./data/userprofile.csv");
 	database->addTable(*table, "Customers");
 
 	table = new Table();
-	loadData(*table, "./data/rating_final.csv");
+	Application::loadData(*table, "./data/rating_final.csv");
 	database->addTable(*table, "Ratings");
 
 	for(;;) {
-		int choice = getMenuSelection();
+		int choice = Application::getMenuSelection();
 		switch(choice) {
 		case -1:
 			{
-				printTables(*database);
+				Application::printTables(*database);
 				break;
 			}
 		case -2:
@@ -58,75 +47,54 @@ int main() {
 				printf("Enter the table name: ");
 				string tableName;
 				cin >> tableName;
-				printRecords(*database, tableName);
+				Application::printRecords(*database, tableName);
 				break;
 			}
-		case MENU::EXIT:
-			{
-				return 0; // Exit program with success code
-			}
-		case MENU::CUSTOMER_INFO:
-			{
-				printf("Enter the customer name: ");
-				string customerName;
-				cin >> customerName;
-				printCustomerInfo(*database, customerName);
-				break;
-			}
-		case MENU::RESTAURANT_INFO:
-			{
-				printf("Enter the restaurant name: ");
-				string restaurantName;
-				cin.get();
-				getline(cin, restaurantName);
-				printRestaurantInfo(*database, restaurantName);
-				break;
-			}
-		case MENU::RESTAURANT_RATINGS:
-			{
-				printf("Enter the restaurant name: ");
-				string restaurantName;
-				cin.get();
-				getline(cin, restaurantName);
-				printRestaurantRatings(*database, restaurantName);
-				break;
-			}
-		case MENU::CUSTOMER_BREAKDOWN:
-			{
-				customerBreakdown(*database);
-				break;
-			}
-		case MENU::CUSTOMER_RATINGS:
-			{
-				printf("Enter the customer name: ");
-				string restaurantName;
-				cin.get();
-				getline(cin, restaurantName);
-				printCustomerRatings(*database, restaurantName);
-				break;
-			}
+		case Application::MENU::EXIT:
+			return 0; // Exit program with success code
+		case Application::MENU::CUSTOMER_INFO:
+			Application::printCustomerInfo(*database);
+			break;			
+		case Application::MENU::RESTAURANT_INFO:
+			Application::printRestaurantInfo(*database);
+			break;
+		case Application::MENU::RESTAURANT_RATINGS:
+			Application::printRestaurantRatings(*database);
+			break;
+		case Application::MENU::CUSTOMER_BREAKDOWN:
+			Application::customerBreakdown(*database);
+			break;
+		case Application::MENU::CUSTOMER_RATINGS:
+			Application::printCustomerRatings(*database);
+			break;
+		case Application::MENU::CUISINE_TYPES:
+			Application::printCuisineTypes(*database);
+			break;
+		case Application::MENU::RESTAURANT_BY_CUISINE:
+			Application::printRestaurantsByCuisine(*database);
+			break;
 		default:
-			{
-				cout << "Invalid choice \n\n" << endl;
-			}
+			cout << "Invalid choice \n\n" << endl;
 		}
 	}
 }
 
-int getMenuSelection() {
+int Application::getMenuSelection() {
 	int choice = 0;
 	string input = "";
 	cout << "---------" << endl;
 	cout << "MAIN MENU" << endl;
 	cout << "---------" << endl;
+	cout << "\t" << "-1 Print the list of tables (DEBUG)" << endl; //Currently for debug purposes
+	cout << "\t" << "-2 Print the records in a table (DEBUG)" << endl; //Currently for debug purposes
+	cout << "\t" << "0) Exit" << endl;
 	cout << "\t" << "1) Print information about a specific customer" << endl;
 	cout << "\t" << "2) Print information about a specific restaurant" << endl;
 	cout << "\t" << "3) Print ratings for a specific restaurant" << endl;
 	cout << "\t" << "4) Print out a breakdown of a certain customer attribute" << endl;
 	cout << "\t" << "5) Print ratings for a specific customer" << endl;
-	cout << "\t" << "0) Exit" << endl;
-	cout << "\t" << "-1) Print the list of tables (DEBUG)" << endl; //Currently for debug purposes
-	cout << "\t" << "-2) Print the records in a table (DEBUG)" << endl; //Currently for debug purposes
+	cout << "\t" << "6) Print a breakdown of cuisine types offered by retaurants" << endl;
+	cout << "\t" << "7) Look up restaurants by cuisine type" << endl;
 	cout << "Enter your selection (number): ";
 	cin >> input;
 	choice = atoi(input.c_str());
@@ -134,7 +102,11 @@ int getMenuSelection() {
 }
 
 // Prints information about a specific customer
-void printCustomerInfo(Database &db, string customerName) {
+void Application::printCustomerInfo(Database &db) {
+	printf("Enter the customer name (case sensitive): ");
+	string customerName;
+	cin >> customerName;
+
 	vector<string> attributes = vector<string>();
 	Table queryResult = db.query(attributes, "Customers", "userID = \"" + customerName + "\"");
 	vector<AttributeList> attr_lists = queryResult.getAttributes();
@@ -152,7 +124,12 @@ void printCustomerInfo(Database &db, string customerName) {
 }
 
 // Print information about a sepcific restaurant
-void printRestaurantInfo(Database &db, string restaurantName) {
+void Application::printRestaurantInfo(Database &db) {
+	printf("Enter the restaurant name (case sensitive): ");
+	string restaurantName;
+	cin.get();
+	getline(cin, restaurantName);
+
 	vector<string> attributes = vector<string>();
 	Table queryResult = db.query(attributes, "Restaurants", "(name = \"" + restaurantName + "\") OR (placeID = \"" + restaurantName + "\")");
 	vector<AttributeList> attr_lists = queryResult.getAttributes();
@@ -171,7 +148,12 @@ void printRestaurantInfo(Database &db, string restaurantName) {
 
 // Prints all the ratings for a restaurant based off name.
 // It grabs the placeID from Restaurants table then querys the Ratings table with the ID
-void printRestaurantRatings(Database &db, string restaurantName) {
+void Application::printRestaurantRatings(Database &db) {
+	printf("Enter the restaurant name (case sensitive): ");
+	string restaurantName;
+	cin.get();
+	getline(cin, restaurantName);
+
 	vector<string> attributes = vector<string>();
 	attributes.push_back("placeID");
 	attributes.push_back("name");
@@ -200,11 +182,11 @@ void printRestaurantRatings(Database &db, string restaurantName) {
 		count++;
 		printf("%-15s %-15s %-15s %-15s \n",  attr_lists[0].getAt(i).c_str(),  attr_lists[2].getAt(i).c_str(),  attr_lists[3].getAt(i).c_str(),  attr_lists[4].getAt(i).c_str());
 	}
-	printf("%-15s %%%-14f %%%-14f %%%-14f \n", "Acceptance:", rating/count/2*100, foodRating/count/2*100, serviceRating/count/2*100);
+	printf("%-15s %%%-14.2f %%%-14.2f %%%-14.2f \n", "Acceptance:", rating/count/2*100, foodRating/count/2*100, serviceRating/count/2*100);
 }
 
 // Lists attributes and presents a percentage breakdown based on that attribute
-void customerBreakdown(Database &db) {
+void Application::customerBreakdown(Database &db) {
 	vector<Table> tables = db.getTables();
 	vector<string> tableNames = db.listTables();
 	int i = 0;
@@ -240,14 +222,18 @@ void customerBreakdown(Database &db) {
 	map<string, float>::iterator iter = frequency.begin();
 	printf("Here is a percentage breakdown of customer's attribute \"%s\"\n", attributes[choice].getName().c_str());
 	while(iter != frequency.end()) {
-		printf("%-25s %f%%\n", iter->first.c_str(), iter->second/total*100);
+		printf("%-25s %5.2f%%\n", iter->first.c_str(), iter->second/total*100);
 		iter++;
 	}
 	printf("\n");
 }
 
 // Prints all the ratings for a customer based off name.
-void printCustomerRatings(Database &db, string customerName) {
+void Application::printCustomerRatings(Database &db) {	
+	printf("Enter the customer name (case sensitive): ");
+	string customerName;
+	cin >> customerName;
+
 	vector<string> attributes = vector<string>();
 	Table queryResult = db.query(attributes, "Ratings", "userID = \"" + customerName + "\"");
 	vector<AttributeList> attr_lists = queryResult.getAttributes();
@@ -265,11 +251,84 @@ void printCustomerRatings(Database &db, string customerName) {
 		count++;
 		printf("%-15s %-15s %-15s %-15s \n",  attr_lists[1].getAt(i).c_str(),  attr_lists[2].getAt(i).c_str(),  attr_lists[3].getAt(i).c_str(),  attr_lists[4].getAt(i).c_str());
 	}
-	printf("%-15s %%%-14f %%%-14f %%%-14f \n", "Acceptance:", rating/count/2*100, foodRating/count/2*100, serviceRating/count/2*100);
+	printf("%-15s %%%-14.2f %%%-14.2f %%%-14.2f \n", "Acceptance:", rating/count/2*100, foodRating/count/2*100, serviceRating/count/2*100);
+}
+
+void Application::printCuisineTypes(Database &db) {
+	vector<Table> tables = db.getTables();
+	vector<string> tableNames = db.listTables();
+	int i = 0;
+	while(tableNames[i].compare("Cuisines") != 0) {
+		i++;
+		if(i >= tableNames.size())
+			break;
+	}
+	vector<AttributeList> attributes = tables[i].getAttributes();
+
+	int choice = 1; // Second column (cuisine type)
+
+	map<string, float> frequency;
+	int total = 0;
+	printf("Working");
+	for(int j = 0; j < tables[i].getSize(); j++) {
+		string item = attributes[choice].getAt(j);
+		frequency[item] = frequency[item] + 1;
+		total++;
+		if(j%30 == 0)
+			printf(".");
+	}
+	printf("\n");
+
+	map<string, float>::iterator iter = frequency.begin();
+	printf("Here is a percentage breakdown of cuisine types\n", attributes[choice].getName().c_str());
+	while(iter != frequency.end()) {
+		printf("%-25s %5.2f%% - %.0f out of %i \n", iter->first.c_str(), iter->second/total*100, iter->second, total);
+		iter++;
+	}
+	printf("\n");
+	printf("Would you like to see restaurants for a specific cuisine type? (yes/no): ");
+	string option;
+	cin >> option;
+	if(option.compare("yes") == 0) {
+		Application::printRestaurantsByCuisine(db);
+	}
+}
+
+void Application::printRestaurantsByCuisine(Database &db) {	
+	printf("What type of cuisine do you want to search for (case sensitive): ");
+	string cuisineType;
+	cin >> cuisineType;
+
+	vector<string> attributes = vector<string>();
+	Table queryResult = db.query(attributes, "Cuisines", "Rcuisine = \"" + cuisineType + "\"");
+	vector<AttributeList> attr_lists = queryResult.getAttributes();
+	if(queryResult.getSize() == 0) {
+		printf("Cuisine query failed. \"%s\" must not be a cuisine type!\n", cuisineType.c_str());
+		return;
+	}
+	
+
+	//Buggy, can't get it to display restaurant name
+	printf("Here is a list of restaurants that have \"%s\" cuisine. \n", cuisineType.c_str());
+	for (int i = 0; i < queryResult.getSize(); ++i) {
+		Table smallQuery = db.query(attributes, "Restaurants", "placeID = \"" + attr_lists[0].getAt(i) + "\"");
+		vector<AttributeList> result = smallQuery.getAttributes();
+		if(smallQuery.getSize() != 0)
+			printf("%s, ", result[0].getAt(0).c_str());
+		else
+			printf("%s, ", attr_lists[0].getAt(i).c_str());
+	}
+	printf("\n");
+	printf("Would you like to get information for a specific restaurant? (yes/no): ");
+	string option;
+	cin >> option;
+	if(option.compare("yes") == 0) {
+		Application::printRestaurantInfo(db);
+	}
 }
 
 // Used for debug purposes
-void printRecords(Database &db, string tableName) {	
+void Application::printRecords(Database &db, string tableName) {	
 	vector<string> tableNames = db.listTables();
 	int index = -1;
 	for(unsigned int i = 0; i < tableNames.size(); i++) {
@@ -295,7 +354,7 @@ void printRecords(Database &db, string tableName) {
 }
 
 // Used for debug purposes
-void printTables(Database &db) {
+void Application::printTables(Database &db) {
 	vector<Table> tables = db.getTables();
 	vector<string> tableNames = db.listTables();
 	for(unsigned int i = 0; i < tables.size(); i++) {
@@ -311,7 +370,7 @@ void printTables(Database &db) {
 }
 
 
-void loadData(Table& destTable, string filename) {
+void Application::loadData(Table& destTable, string filename) {
 	printf("Loading filename %s \n", filename.c_str());
 	//TODO - build a table from the csv file.
 	ifstream file = ifstream(filename.c_str());
---
+++ DatabastApplication/DatabastApplication/Application.h
@@ -0,0 +1,17 @@
+#include "API.h"
+
+class Application {
+public:
+	static int getMenuSelection(); // Shows the menu and gets the choice
+	static void loadData(Table& destTable, string filename); // Loads a table from a .csv
+	static void printTables(Database &db);
+	static void printRecords(Database &db, string tableName);
+	static void printCustomerInfo(Database &db);
+	static void printRestaurantInfo(Database &db);
+	static void printRestaurantRatings(Database &db);
+	static void customerBreakdown(Database &db);
+	static void printCustomerRatings(Database &db);
+	static void printCuisineTypes(Database &db);
+	static void printRestaurantsByCuisine(Database &db);
+	enum MENU { EXIT, CUSTOMER_INFO, RESTAURANT_INFO, RESTAURANT_RATINGS, CUSTOMER_BREAKDOWN, CUSTOMER_RATINGS, CUISINE_TYPES, RESTAURANT_BY_CUISINE };
+};
